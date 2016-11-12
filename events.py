ce0934042aa8ee2e3cd071888e8108794ccb8014
Kerby Shedden <kshedden@umich.edu>
2016-11-12 11:06:12 -0500

    Improve commenting

--- events.py
+++ events.py
@@ -6,18 +6,28 @@ import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 
+# Path to data file
 path = "/nfs/kshedden/Lance_Sloan/pr.json.gz"
 
+# Load the raw json data
 all_recs = []
 with gzip.open(path, "rt") as gid:
     all_recs = json.load(gid)
 
-agg = {}
-
+# strptime for posix time stamps
 tfmt = "%Y-%m-%dT%H:%M:%S.%fZ"
 
+# Extract some fields for each event; agg is a map from course names
+# to event time stamps, the event times are split into columns
+# according to the time.struct_time fields:
+# https://docs.python.org/3/library/time.html#time.struct_time
+agg = {}
 for rec in all_recs:
+
+    # This seems to be where the good information is
     js = json.loads(rec['raw'])
+
+    # Extract the user id/, but not currently used here
     if 'actor' not in js or js['actor'] is None:
         continue
     actor = js['actor']
@@ -25,6 +35,8 @@ for rec in all_recs:
     user = user_url.split(":")
     if len(user) < 3:
         continue
+
+    # Extract the time
     if 'group' not in js or js['group'] is None:
         continue
     course = js['group']['name']
@@ -36,8 +48,6 @@ for rec in all_recs:
     agg[course].append(list(etime.timetuple()))
 
 
-
-
 pdf = PdfPages("events.pdf")
 
 xlabels = {6: "Day of week", 7: "Date", 3: "Hour"}
@@ -46,6 +56,7 @@ for course in agg.keys():
 
     mat = np.asarray(agg[course])
 
+    # Three types of plots: by day, by weekday, and by hour
     for col in 6, 7, 3:
 
         u, c = np.unique(mat[:, col], return_counts=True)
@@ -58,16 +69,17 @@ for course in agg.keys():
         plt.clf()
         ax = plt.axes()
 
-        if col == 7:
+        if col == 6:
+            ax.xaxis.set_ticks([0, 1, 2, 3, 4, 5, 6])
+            ax.xaxis.set_ticklabels(["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"])
+            ax.set_xlim(-0.2, 6.2)
+        elif col == 7:
             months = mdates.MonthLocator()
             days = mdates.DayLocator(bymonthday=[1, 7, 14, 21, 28])
             months_fmt = mdates.DateFormatter('%b')
             ax.xaxis.set_major_locator(months)
             ax.xaxis.set_minor_locator(days)
             ax.xaxis.set_major_formatter(months_fmt)
-        elif col == 6:
-            ax.xaxis.set_ticks([0, 1, 2, 3, 4, 5, 6])
-            ax.xaxis.set_ticklabels(["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"])
         elif col == 3:
             ax.xaxis.set_ticks(range(0, 24, 3))
 
@@ -77,9 +89,6 @@ for course in agg.keys():
 
         ax.plot(d, c, '-', color='grey')
         ax.plot(d, c, 'o', color='orange', alpha=0.8)
-
-        if col == 6:
-            ax.set_xlim(-0.2, 6.2)
 
         ax.set_ylabel("Records", size=15)
         pdf.savefig()
