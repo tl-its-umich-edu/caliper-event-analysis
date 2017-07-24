a383679159410b11d65cb7afedf27a9660ebe997
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2017-07-24 12:06:20 -0400

    TLCALIPER-115 - Keep only unique event data

--- src/reconcile/caliperEventTsv.sh
+++ src/reconcile/caliperEventTsv.sh
@@ -3,4 +3,4 @@
 # Send input through stdin or give a filename as an argument
 
 jq -r '[.eventTime[:19]+"Z", .actor.name, .object["@id"], .generated.value//"0"]|@tsv' ${1:--} | \
-    sort | sed 's/%40/@/g'
+    sort -u | sed 's/%40/@/g'
