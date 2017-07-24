a458d68b4e3ff2364f26bf8acb43c3c6eeda93d8
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2017-07-24 12:11:30 -0400

    TLCALIPER-115 - Use a temp. file
    By using a temporary file, we can monitor the activity and progress of the program.

--- src/reconcile/caliperEventTsv.sh
+++ src/reconcile/caliperEventTsv.sh
@@ -1,6 +1,9 @@
 #!/bin/sh --
 
+UNSORTED_DATA="unsorted_${$}.tsv"
+
 # Send input through stdin or give a filename as an argument
 
-jq -r '[.eventTime[:19]+"Z", .actor.name, .object["@id"], .generated.value//"0"]|@tsv' ${1:--} | \
-    sort -u | sed 's/%40/@/g'
+jq -r '[.eventTime[:19]+"Z", .actor.name, .object["@id"], .generated.value//"0"]|@tsv' ${1:--} > ${UNSORTED_DATA}
+sort -u ${UNSORTED_DATA} | sed 's/%40/@/g'
+rm ${UNSORTED_DATA}
