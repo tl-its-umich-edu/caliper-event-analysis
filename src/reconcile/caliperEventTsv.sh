976ca67fe33a285324ec6d4c39f805a7ff5d7a7b
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2017-08-02 22:22:12 +0000

    TLCALIPER-118 - Caliper TSV corrections
    Learned later that the end timestamps from Problem Roulette are not used for the `eventTime` property, but for the `generated.attempt.endedAtTime` property instead.  Also removed the step to eliminate duplicate rows of data.

--- src/reconcile/caliperEventTsv.sh
+++ src/reconcile/caliperEventTsv.sh
@@ -4,6 +4,7 @@ UNSORTED_DATA="unsorted_${$}.tsv"
 
 # Send input through stdin or give a filename as an argument
 
-jq -r '[.eventTime[:19]+"Z", .actor.name, .object["@id"], .generated.value//"0"]|@tsv' ${1:--} > ${UNSORTED_DATA}
-sort -u ${UNSORTED_DATA} | sed 's/%40/@/g'
+jq -r '[.generated.attempt.endedAtTime[:19]+"Z", .actor.name, .object["@id"], .generated.value//"0"]|@tsv' ${1:--} > ${UNSORTED_DATA}
+
+sort ${UNSORTED_DATA} | sed 's/%40/@/g'
 rm ${UNSORTED_DATA}
