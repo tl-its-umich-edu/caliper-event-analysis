3abef99a27030787e4602f8cedc3558f2c31be5d
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2017-08-03 20:15:44 +0000

    TLCALIPER-118 - Added property sort
    Added sorting of properties to the command as well as accepting a second argument with an optional filename to be sorted.

--- src/reconcile/jsonlSort.sh
+++ src/reconcile/jsonlSort.sh
@@ -1,11 +1,12 @@
 #!/bin/sh --
 
-# Execute with an argument containing the name of the property to be
-# used for sorting.
+# Execute with arguments:
+# 1. (Required) Name of the property to be used for sorting.
+# 2. (Optional) Name of the input file to be sorted. Default: stdin
 
-# Improved according to a suggestion from @pkoppstein in response to my
-# support issue:
+# Improved according to a suggestion from @pkoppstein
+# in response to support issue:
 # https://github.com/stedolan/jq/issues/1447#issuecomment-314918635
 
-jq --slurp --compact-output 'sort_by(.'${1}')[]'
+jq --slurp --compact-output --sort-keys 'sort_by(.'${1}')[]' ${2:--}
 
