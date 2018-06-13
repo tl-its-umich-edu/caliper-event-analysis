0ca5a6d1678fa2af1926160e6a6214669ae5b9ed
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2018-06-13 15:59:31 -0400

    cleanup prior to notebooking
    I made a few changes to test the program from the CLI before porting it to Jupyter Notebook.  Mostly insignificant, I changed the name of the input file and did a global replace of unnecessary double quotes with singles.

--- src/analyze/events.py
+++ src/analyze/events.py
@@ -8,15 +8,15 @@ from matplotlib.backends.backend_pdf import PdfPages
 import pandas as pd
 
 # Path to data file
-path = "/nfs/kshedden/Lance_Sloan/pr.json.gz"
+path = '../../tmp/pr.json.gz'
 
 # Load the raw json data
 all_recs = []
-with gzip.open(path, "rt") as gid:
+with gzip.open(path, 'rt') as gid:
     all_recs = json.load(gid)
 
 # strptime for posix time stamps
-tfmt = "%Y-%m-%dT%H:%M:%S.%fZ"
+tfmt = '%Y-%m-%dT%H:%M:%S.%fZ'
 
 # Extract some fields for each event; agg is a map from course names
 # to event time stamps, the event times are split into columns
@@ -33,8 +33,8 @@ for rec in all_recs:
     if 'actor' not in js or js['actor'] is None:
         continue
     actor = js['actor']
-    user_url = actor["@id"]
-    user = user_url.split(":")
+    user_url = actor['@id']
+    user = user_url.split(':')
     if len(user) < 3:
         continue
 
@@ -50,20 +50,20 @@ for rec in all_recs:
     agg.append(etime)
 
 agg = pd.DataFrame(agg)
-agg.columns = tfields + ["course"]
+agg.columns = tfields + ['course']
 
-pdf = PdfPages("events.pdf")
+pdf = PdfPages('events.pdf')
 
-xlabels = {"tm_wday": "Day of week", "tm_yday": "Date", "tm_hour": "Hour"}
+xlabels = {'tm_wday': 'Day of week', 'tm_yday': 'Date', 'tm_hour': 'Hour'}
 
-for course, mat in agg.groupby("course"):
+for course, mat in agg.groupby('course'):
 
     # Three types of plots: by day, by weekday, and by hour
-    for col in "tm_yday", "tm_wday", "tm_hour":
+    for col in 'tm_yday', 'tm_wday', 'tm_hour':
 
         vc = mat[col].value_counts().sort_index()
 
-        if col == "tm_yday":
+        if col == 'tm_yday':
             d = [datetime.fromordinal(x) for x in vc.index]
         else:
             d = vc.index.tolist()
@@ -71,18 +71,18 @@ for course, mat in agg.groupby("course"):
         plt.clf()
         ax = plt.axes()
 
-        if col == "tm_wday":
+        if col == 'tm_wday':
             ax.xaxis.set_ticks([0, 1, 2, 3, 4, 5, 6])
-            ax.xaxis.set_ticklabels(["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"])
+            ax.xaxis.set_ticklabels(['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'])
             ax.set_xlim(-0.2, 6.2)
-        elif col == "tm_yday":
+        elif col == 'tm_yday':
             months = mdates.MonthLocator()
             days = mdates.DayLocator(bymonthday=[1, 7, 14, 21, 28])
             months_fmt = mdates.DateFormatter('%b')
             ax.xaxis.set_major_locator(months)
             ax.xaxis.set_minor_locator(days)
             ax.xaxis.set_major_formatter(months_fmt)
-        elif col == "tm_hour":
+        elif col == 'tm_hour':
             ax.xaxis.set_ticks(range(0, 24, 3))
 
         ax.set_title(course)
@@ -92,7 +92,7 @@ for course, mat in agg.groupby("course"):
         ax.plot(d, vc.values, '-', color='grey')
         ax.plot(d, vc.values, 'o', color='orange', alpha=0.8)
 
-        ax.set_ylabel("Records", size=15)
+        ax.set_ylabel('Records', size=15)
         pdf.savefig()
 
 pdf.close()
